f08fcc6ca4d388a8cddc6246f918686277cb381e
Mike Wadsten <mikewadsten@gmail.com>
2013-01-08 10:45:34 -0600

    Remove dupes, clean up outputs, stuff like that.

--- gis.py
+++ gis.py
@@ -27,18 +27,30 @@ def get_ids(lake_name='', lake_num=0, min_val=0):
     get the list of matching object ids."""
     params = {'returnIdsOnly': True, 'f': 'json'}
 
+    try:
+        min_val = int(min_val)
+    except ValueError:
+        print "Invalid minimum value:", min_val
+        sys.exit(1)
+
     if lake_name:
         params['where'] = WHERE_LKNAME_FMT.format(lake_name, min_val)
     elif lake_num:
+        try:
+            lake_num = int(lake_num)
+        except ValueError:
+            print "Invalid lake number value:", lake_num
+            sys.exit(1)
         params['where'] = WHERE_LKNUM_FMT.format(lake_num, min_val)
     else:
-        print "Must specify either a lake name or lake number."
+        print "Must specify either a lake name or lake number, dumbass."
         sys.exit(1)
 
-    print "Request to GIS server being made..."
+    sys.stdout.write("Making request to GIS server...")
+    sys.stdout.flush()  # push text out
     resp = requests.get(BASE_URL, params=params)
-    print resp.url
-    print "Request to GIS server has completed."
+    #print resp.url
+    sys.stdout.write(" Done.\n")
     return json.loads(resp.text).get('objectIds', [])  # probably unreliable...
 
 def get_data(ids=[]):
@@ -49,12 +61,17 @@ def get_data(ids=[]):
     recvd = 0
     features = []
     ids = map(str, ids)  # ids are initially integers.
+    chunksz = 100
     while recvd < count:
-        _ids = ids[:30]
-        ids = ids[30:]
+        _ids = ids[:chunksz]
+        ids = ids[chunksz:]
         params['objectIds'] = ','.join(_ids)
-        print "Request for properties", recvd, "to", recvd + len(_ids), "in progress."
+        out = "Request for properties {} to {}...".format(
+                            recvd + 1, recvd + len(_ids))
+        sys.stdout.write(out)
+        sys.stdout.flush()  # push text out
         resp = requests.get(BASE_URL, params=params)
+        sys.stdout.write(" Done.\n")
         if resp.status_code != 200:
             print "Request came back with status", resp.status_code
         else:
@@ -68,21 +85,52 @@ def get_data(ids=[]):
 
 def cull_the_herd(features=[]):
     """Remove duplicate names/addresses."""
-    # TODO
-    return features
+    def make_hasher(t):
+        return '|'.join([ t['OWNAME'], t['OWADR1'], t['OWADR2'],
+                          t['OWADR3'], t['OWADR4'] ])
+
+    hash_to_dicts = {}
+    retval = []
+
+    for d in features:
+        h = make_hasher(d)
+        hash_to_dicts.setdefault(h, d)
+
+    # Get the addresses out in alphabetical order by owner name?
+    for key in sorted(hash_to_dicts.keys()):
+        retval += [ hash_to_dicts[key] ]
+
+    return retval
+
+#####################################################################
+# I/O begins here
+
+print "Welcome to Mike's Crow Wing County GIS searcher thing..."
+print "NOTE: Leave 'Lake name' empty if you want to provide a lake number."
+print "NOTE: If 'Lake name' is non-empty, that is used instead of lake number."
+print
 
 ids = get_ids(lake_name=raw_input('Lake name: '),
-              min_val=int(raw_input('Value: ')))
-print len(ids)
+              lake_num=raw_input('Lake number: '),
+              min_val=raw_input('Value: '))
+
+print
+print "Count for initial query:", len(ids)
 data = get_data(ids)
 print "Initial count:", len(data)
 data = cull_the_herd(data)
 print "Count after duplicate removal:", len(data)
 print
 
+if len(data) < 1:
+    # No data to speak of.
+    print "No entries were returned. Goodbye!"
+    sys.exit(0)
+
 print "Writing data out to out.csv"
 with open('out.csv', 'w') as f:
-    writer = csv.DictWriter(f, fieldnames=data[0].keys(),
+    fieldnames = ["OWNAME", "OWADR1", "OWADR2", "OWADR3", "OWADR4"]
+    writer = csv.DictWriter(f, fieldnames=fieldnames,
                             delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_MINIMAL)
     writer.writeheader()
